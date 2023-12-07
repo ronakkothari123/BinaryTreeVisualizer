a4f2ffa693276d30a56e0d5d058a9fb0c601555f
Ronak Kothari <rk.ronakjkothari@gmail.com>
2023-12-07 00:23:03 -0800

    GUI Update PT1
    THIS IS COOL

---
+++ src/Components/CirclePanel.java
@@ -0,0 +1,14 @@
+package Components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CirclePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics g) {
+        //Adding  super.paintComponent....
+        super.paintComponent(g);
+
+        g.drawOval(0, 0, g.getClipBounds().width, g.getClipBounds().height);
+    }
+}
